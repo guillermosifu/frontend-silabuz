f2522ed1d350b521eba2bcebb41daf67caf41e31
guillermosifu <sifuentes1201@gmail.com>
2023-06-05 22:02:11 -0500

    esto es js

---
+++ semana4/archivo.js
@@ -0,0 +1,97 @@
+
+
+console.log("hola como estan")
+
+// la variables son solo contenedores... alamacenan una informacion o tipod e dato 
+
+
+const numero1=10
+const numero2=20
+
+resultado = 0
+
+resultado=numero1+numero2
+
+console.log(resultado)
+
+//recuerden 
+//variable siempre va definir un dato ...
+//alert  -- estyablece de forma poredeterminada con los estilos del navegador
+// para imprimir en nuestra console   useamos console.log
+
+
+console.error("error",resultado)
+
+
+//calcauladora
+
+//prompt  es una ventana emergente esl cuial puede recepsciona informacion
+//y ese valor ingresasdo de debe guradr en una variable
+//const valor1=prompt("ingresa el valor");
+//const valor2=prompt("ingresa el valor 2")
+
+//const operacion = prompt("ingresa una operacion puede ser + - x /");
+
+//necesiatamos que el usuario ingrese el simbolo de operacion a ejecutar 
+
+let resultadoOperacion=0
+
+//para poder hacer los calculos debo convertir mi vairible string a numerico 
+
+//es conm ParseInt ---  
+//  + delante de la vairbale que almaccena esa info
+
+// if(operacion === "+"){
+
+//   resultadoOperacion= +valor1 + +valor2;
+//   console.log("resultado es", resultadoOperacion)
+// }else if(operacion==="-"){
+//     resultadoOperacion= +valor1 - +valor2;
+//   console.log("resultado es", resultadoOperacion)
+// }else if(operacion==="x"){
+//     resultadoOperacion= +valor1 * +valor2;
+//   console.log("resultado es", resultadoOperacion)
+// }else if (operacion==="/"){
+//  if(+valor2!==0){
+//      resultadoOperacion = +valor1/+valor2;
+//      console.log("resultado division ",resultadoOperacion)
+//  }else{
+//      console.warn("el divisor debe ser distinto a 0");
+//  }
+// }else{
+//      console.error("operacion no valida")
+//  }
+
+//cmo hacer un algoritmo que detecte ei un numero es par o impar
+
+const numeroMultiplo =16
+
+//para poder haceeste ejericio nosotros uaremos al llamado residual 
+// %    modulo = %
+
+if (numeroMultiplo % 2 === 0){
+    console.log('numero par ',numeroMultiplo)
+}
+if(numeroMultiplo % 3 === 0){
+    console.log('numero multiplo de 3',numeroMultiplo)
+}
+////////////////////////////////////////////////////////
+//ESCRIBAN UN ALGORITMO QUE PIDA 2 NUMEROS  USAR PROMPT
+// DETECTE EL NUMERO MAYOR//
+
+//   const numeroOne=+prompt('ingrese numero uno')
+//   const numeroTwo=+prompt('ingrese numero dos')
+
+//   let mensajeRes= 'numero 2 es mayor '
+//   if(numeroOne>numeroTwo){
+//       mensajeRes='numero uno es mayor'
+//   }
+
+//   console.log(mensajeRes)
+
+
+  // vamo a conocer el uso del and or  &&   || 
+  //if o else
+
+
+  
