725c34c6fb6e4588b9cd0b6f735095b1bd49ed94
guillermosifu <sifuentes1201@gmail.com>
2023-06-06 20:56:30 -0500

    objetc

---
+++ semana4/dia2/objetos.js
@@ -0,0 +1,35 @@
+//  un objeto es un etidne independiete , tiene propiedas
+//tiene atributos..  
+
+// un objeto key ,value 
+
+//timon,ruedad, colores, frenos 
+
+const carro ={
+    color : "azul",
+    motor: 3.0,
+    marca: "susuki",
+    nuevo: true
+}
+
+carro.marca="tesla"
+carro.motor="elctrico"
+carro.frenos="abs"
+
+console.table(carro)
+//de modificar un dato?
+
+// objeto persona
+
+const persona={
+    
+    nombre:"pepito",
+    sexo: "maculino",
+    edad:35,
+    talla:1.78
+}
+
+persona.nombre="piero"
+    
+
+console.table(persona)
