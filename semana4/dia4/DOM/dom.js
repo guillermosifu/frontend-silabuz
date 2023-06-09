f0a75a957d03d2a70a66999ddb98635a6d552aff
guillermosifu <sifuentes1201@gmail.com>
2023-06-08 22:25:06 -0500

    js

---
+++ semana4/dia4/DOM/dom.js
@@ -0,0 +1,16 @@
+// existe 5 selcetor pars manipular 
+
+//por tag 
+//[por clase]
+//id
+//extor global
+
+
+const h1 = document.getElementsByTagName("h1")
+
+const Input =document.querySelector("#input_email")
+const Input2=document.querySelector(".input_name")
+const button=document.querySelector(".btn")
+
+Input.style.backgroundColor = "blue";
+Input.style.color = "red",
---
+++ semana4/dia4/eventos/main.js
@@ -0,0 +1,9 @@
+
+const imprimir =()=>{
+    console.log("este s desde js")
+}
+
+//atrapemos el boton y pongale su evento
+
+const button = document.querySelector("#atrapame");
+
